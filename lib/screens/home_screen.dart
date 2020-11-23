cb690b180307f8ec28e2d30e31d72018b1cbe0ee
sannidhya-kushwaha <sannidhyakushwaha@gmail.com>
2020-11-23 14:42:17 +0530

    Home screen  appBar

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,35 @@
+import 'package:Food_Delivery_App/data/data.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.account_circle),
+          iconSize: 30,
+          onPressed: () {},
+        ),
+        title: Text('Food Delivery'),
+        actions: [
+          FlatButton(
+            child: Text(
+              'Cart (${currentUser.cart.length})',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20.0,
+              ),
+            ),
+            onPressed: () {},
+          )
+        ],
+      ),
+    );
+  }
+}
