68f108de5fd25924b116fc0a664892380a20b0f8
sannidhya-kushwaha <sannidhyakushwaha@gmail.com>
2020-11-23 15:00:10 +0530

    Adding Search Bar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -27,6 +27,45 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
             ),
             onPressed: () {},
+          ),
+        ],
+      ),
+      body: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: TextField(
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.symmetric(vertical: 15.0),
+                fillColor: Colors.white,
+                filled: true,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30.0),
+                  borderSide: BorderSide(
+                    width: 0.8,
+                  ),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30.0),
+                  borderSide: BorderSide(
+                    width: 0.8,
+                    color: Theme.of(context).primaryColor,
+                  ),
+                ),
+                // hintText: 'Search Food or Restaurants',
+                labelText: 'Search Food or Restaurants',
+                prefixIcon: Icon(
+                  Icons.search,
+                  size: 30.0,
+                ),
+                suffixIcon: IconButton(
+                  icon: Icon(
+                    Icons.clear,
+                  ),
+                  onPressed: () {},
+                ),
+              ),
+            ),
           )
         ],
       ),
