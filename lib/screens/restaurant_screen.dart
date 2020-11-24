88208267b10d801ed993aa6dbd561ca4bb5aa4f2
sannidhya-kushwaha <sannidhyakushwaha@gmail.com>
2020-11-24 13:51:08 +0530

    GridView Menu

--- lib/screens/restaurant_screen.dart
+++ lib/screens/restaurant_screen.dart
@@ -1,3 +1,4 @@
+import 'package:Food_Delivery_App/models/food.dart';
 import 'package:Food_Delivery_App/models/restaurant.dart';
 import 'package:Food_Delivery_App/widgets/rating_stars.dart';
 import 'package:flutter/material.dart';
@@ -12,6 +13,85 @@ class RestaurantScreen extends StatefulWidget {
 }
 
 class _RestaurantScreenState extends State<RestaurantScreen> {
+  _buildMenuIten(Food menuItem) {
+    return Center(
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          Container(
+            height: 145.0,
+            width: 145.0,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(menuItem.imageUrl),
+                fit: BoxFit.cover,
+              ),
+              borderRadius: BorderRadius.circular(15.0),
+            ),
+          ),
+          Container(
+            height: 145.0,
+            width: 145.0,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15.0),
+              gradient: LinearGradient(
+                begin: Alignment.centerRight,
+                end: Alignment.bottomLeft,
+                colors: [
+                  Colors.black.withOpacity(0.3),
+                  Colors.black87.withOpacity(0.3),
+                  Colors.black54.withOpacity(0.3),
+                  Colors.black38.withOpacity(0.3),
+                ],
+                stops: [0.1, 0.4, 0.6, 0.9],
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 65.0,
+            child: Column(
+              children: [
+                Text(
+                  menuItem.name,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 1.2,
+                  ),
+                ),
+                Text(
+                  '\$${menuItem.price.toString()}',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 1.2,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            bottom: 10.0,
+            right: 10.0,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Theme.of(context).primaryColor,
+                borderRadius: BorderRadius.circular(30.0),
+              ),
+              child: IconButton(
+                icon: Icon(Icons.add),
+                iconSize: 30.0,
+                onPressed: () {},
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,7 +102,7 @@ class _RestaurantScreenState extends State<RestaurantScreen> {
               Hero(
                 tag: widget.restaurant.imageUrl,
                 child: Image(
-                  height: 220.0,
+                  height: 200.0,
                   width: MediaQuery.of(context).size.width,
                   image: AssetImage(widget.restaurant.imageUrl),
                   fit: BoxFit.cover,
@@ -52,7 +132,7 @@ class _RestaurantScreenState extends State<RestaurantScreen> {
             ],
           ),
           Padding(
-            padding: const EdgeInsets.all(20.0),
+            padding: EdgeInsets.all(20.0),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
@@ -62,23 +142,22 @@ class _RestaurantScreenState extends State<RestaurantScreen> {
                     Text(
                       widget.restaurant.name,
                       style: TextStyle(
-                        fontSize: 22.0,
+                        fontSize: 20.0,
                         fontWeight: FontWeight.w600,
                       ),
                     ),
                     Text(
                       '0.2 miles away',
                       style: TextStyle(
-                        fontSize: 18.0,
+                        fontSize: 16.0,
                       ),
                     ),
                   ],
                 ),
                 RatingStars(widget.restaurant.rating),
-                SizedBox(height: 6.0),
                 Text(
                   widget.restaurant.address,
-                  style: TextStyle(fontSize: 18.0),
+                  style: TextStyle(fontSize: 16.0),
                 ),
               ],
             ),
@@ -112,6 +191,30 @@ class _RestaurantScreenState extends State<RestaurantScreen> {
               ),
             ],
           ),
+          Center(
+            child: Text(
+              'Menu',
+              style: TextStyle(
+                fontSize: 22.0,
+                fontWeight: FontWeight.w600,
+                letterSpacing: 1.2,
+              ),
+            ),
+          ),
+          SizedBox(height: 4),
+          Expanded(
+            child: GridView.count(
+              padding: EdgeInsets.all(10.0),
+              crossAxisCount: 2,
+              children: List.generate(
+                widget.restaurant.menu.length,
+                (index) {
+                  Food food = widget.restaurant.menu[index];
+                  return _buildMenuIten(food);
+                },
+              ),
+            ),
+          )
         ],
       ),
     );
